e871632efbd1e18bbd9fc624e4d0a36ee0bb30fa
hieuPhan09 <132120825+hieuPhan09@users.noreply.github.com>
2025-02-18 20:55:27 -0800

    Update_from_HP_calcFuncs.py

--- calcFuncs.py
+++ calcFuncs.py
@@ -4,32 +4,6 @@
 #[x] Addition function
 #[x] Division function
 
-#HP
-#[] Subtraction function
-#[] Write function to loop the program. 
-    #Once the program ends, ask user if they want to run the program again (Y/N)
-    #If user enters Y, run the program again
-    #If user enters N, end the program
-
-#Amine
-#[] Multiplication function
-#[] Write function to check if result is odd or even
-
-#menu
-def menu(operator_str):
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-    if operator_str == "+":
-        print(addition(num1, num2))
-    elif operator_str == "-":
-        print(subtraction(num1, num2))
-    elif operator_str == "*":
-        print(multiplication(num1, num2))
-    elif operator_str == "/":
-        print(division(num1, num2))
-    else:
-        print("Invalid choice!")
-
 #addition
 def addition(num1, num2):
     sum = num1 + num2
@@ -38,8 +12,62 @@ def addition(num1, num2):
 def division(num1, num2):
     result = num1 / num2
     return result
-#subtraction
+
+#Amine
+#[] Multiplication function
+def multiplication(num1, num2):
+    return num1 * num2
+#[] Write function to check if result is odd or even
+
+###########HP
+#[] Subtraction function
+def subtraction(num1, num2):
+    return num1 - num2
+
+#[] Write function to loop the program.
+def calculator():
+    num1 = float(input("Enter first number: "))
+    print("'+'\n'-'\n'*'\n'/'")
+    operator_str = input("Choose your operation: ")
+    should_continue = True
+    result = 0.0
     
-#multiplication
+    while should_continue:
+        num2 = float(input("Enter the next number: "))
+        if operator_str == '+':
+            result = addition(num1, num2)
+        elif operator_str == '-':
+            result = subtraction(num1, num2)
+        elif operator_str == '*':
+            result = multiplication(num1, num2)
+        elif operator_str == '/':
+            result = division(num1, num2)
+        else:
+            print("Invalid choice!")
+        print(f"{num1} {operator_str} {num2} = {result}")
+     #Once the program ends, ask user if they want to run the program again (Y/N)
+        run_again = input("Do you want to have a new calculation? (y/n): ").lower()
+       
+        if run_again == 'y':   #If user enters Y, run the program again
+            calculator()
+        else:  #If user enters N, end the program
+            should_continue = False
+
+#menu
+
+# def menu(operator_str):
+#     num1 = int(input("Enter first number: "))
+#     num2 = int(input("Enter second number: "))
+#     if operator_str == "+":
+#         print(addition(num1, num2))
+#     elif operator_str == "-":
+#         print(subtraction(num1, num2))
+#     elif operator_str == "*":
+#         print(multiplication(num1, num2))
+#     elif operator_str == "/":
+#         print(division(num1, num2))
+#     else:
+#         print("Invalid choice!")
+
 
     
